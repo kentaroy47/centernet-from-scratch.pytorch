a2b9e126448130824152363257162eaccf3f84c1
kentaroy47 <kentaroy@stanford.edu>
2020-03-12 02:51:08 +0000

    update focal loss for centernet

--- utils/focalloss.py
+++ utils/focalloss.py
@@ -8,117 +8,51 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 #from utils import one_hot_embedding
 
-def one_hot_embedding(labels, num_classes):
-    """
-    Embedding labels to one-hot form.
-    Args:
-    :param labels: (LongTensor) class label, sized [N,].
-    :param num_classes: (int) number of classes.
-    :return:
-            (tensor) encoded labels, size [N, #classes].
-    """
-    y = torch.eye(num_classes)  # [D, D]
-    return y[labels]  # [N, D]
+# focal loss
+def neg_loss(pred, gt):
+  ''' Modified focal loss. Exactly the same as CornerNet.
+      Runs faster and costs a little bit more memory
+    Arguments:
+      pred (batch x c x h x w)
+      gt_regr (batch x c x h x w)
+  '''
+  pred = pred.unsqueeze(1).float()
+  gt = gt.unsqueeze(1).float()
 
-class FocalLoss(nn.Module):
-    def __init__(self, num_classes=80):
-        super(FocalLoss, self).__init__()
-        self.num_classes = num_classes
+  pos_inds = gt.eq(1).float()
+  neg_inds = gt.lt(1).float()
+  neg_weights = torch.pow(1 - gt, 4)
 
-    def focal_loss(self, x, y):
-        """
-        Focal loss.
-        Args:
-        :param x: (tensor) sized [N, D]
-        :param y: (tensor) sized [N, ].
-        :return:
-                (tensor) focal loss.
-        """
-        alpha = 0.25
-        gamma = 2
+  loss = 0
 
-        t = one_hot_embedding(y.data.cpu().long(), 1 + self.num_classes)  # [N, 81]
-        t = t[:, 1:]  # exclude background
-        t = Variable(t).cuda()
+  pos_loss = torch.log(pred + 1e-12) * torch.pow(1 - pred, 3) * pos_inds
+  neg_loss = torch.log(1 - pred + 1e-12) * torch.pow(pred, 3) * neg_weights * neg_inds
 
-        p = x.sigmoid()
-        pt = p * t + (1 - p) * (1 - t)
-        w = alpha * t + (1 - alpha) * (1 - t)
-        w = w * (1 - pt).pow(gamma)
-        return F.binary_cross_entropy_with_logits(x, t, w.detach(), size_average=False)
+  num_pos  = pos_inds.float().sum()
+  pos_loss = pos_loss.sum()
+  neg_loss = neg_loss.sum()
 
-    def focal_loss_alt(self, x, y):
-        """
-        Focal loss alternative.
-        Args:
-        :param x: (tensor) sized [N, D]
-        :param y: (tensor) sized [N, ].
-        :return:
-                (tensor) focal loss.
-        """
-        alpha = 0.25
+  if num_pos == 0:
+    loss = loss - neg_loss
+  else:
+    loss = loss - (pos_loss + neg_loss) / num_pos
+  return loss
 
-        t = one_hot_embedding(y.data.cpu().long(), 1 + self.num_classes)  # [N, 81]
-        t = t[:, 1:]  # exclude background
-        t = Variable(t).cuda()
-
-        xt = x * (2 * t - 1)  # xt = x if t>0 else -x
-        pt = (2 * xt + 1).sigmoid()
-
-        w = alpha * t + (1 - alpha) * (1 - t)
-        loss = -w * pt.log() / 2
-        return loss.sum()
-
-    @staticmethod
-    def where(cond, x_1, x_2):
-        return (cond.float() * x_1) + ((1 - cond.float()) * x_2)
-
-    def forward(self, loc_preds, loc_targets, cls_preds, cls_targets):
-        """
-        Compute loss between (loc_preds, loc_targets) and (cls_preds, cls_targets).
-        Args:
-        :param loc_preds: (tensor) predicted locations, sized [batch_size, #anchors, 4].
-        :param loc_targets: (tensor) encoded target locations, sized [batch_size, #anchors, 4].
-        :param cls_preds: (tensor) predicted class confidences, sized [batch_size, #anchors, #classes].
-        :param cls_targets: (tensor) encoded target labels, sized [batch_size, #anchors].
-        loss:
-            (tensor) loss = SmoothL1Loss(loc_preds, loc_targets) + FocalLoss(cls_preds, cls_targets).
-        """
-        # print(cls_targets)
-        batch_size, num_boxes = cls_targets.size()
-        pos = cls_targets > 0  # [N, #anchors]
-        num_pos = pos.data.long().sum()
-        # print(num_pos, 'num_pos')
-
-        ##########################################################
-        # loc_loss = SmoothL1Loss(pos_loc_preds, pos_loc_targets)
-        ##########################################################
-        if num_pos > 0:
-            mask = pos.unsqueeze(2).expand_as(loc_preds)  # [N, #anchors, 4]
-            masked_loc_preds = loc_preds[mask].view(-1, 4)  # [#pos, 4]
-            masked_loc_targets = loc_targets[mask].view(-1, 4)  # [#pos, 4]
-            # loc_loss = F.smooth_l1_loss(masked_loc_preds, masked_loc_targets, size_average=False)
-            regression_diff = torch.abs(masked_loc_targets - masked_loc_preds)
-            loc_loss = self.where(torch.le(regression_diff, 1.0 / 9.0), 0.5 * 9.0 * torch.pow(regression_diff, 2),
-                                  regression_diff - 0.5 / 9.0)
-            # use mean() here, so the loc_loss dont have to divide num_pos
-            # loc_loss = loc_loss.sum()
-            loc_loss = loc_loss.mean()
-        else:
-            num_pos = 1.
-            loc_loss = Variable(torch.Tensor([0]).float().cuda())
-
-        ##########################################################
-        # cls_loss = FocalLoss(cls_preds, cls_targets)
-        ##########################################################
-        pos_neg = cls_targets > -1  # exclude ignored anchors
-        mask = pos_neg.unsqueeze(2).expand_as(cls_preds)
-        masked_cls_preds = cls_preds[mask].view(-1, self.num_classes)
-        cls_loss = self.focal_loss(masked_cls_preds, cls_targets[pos_neg])
-
-        # print('loc_loss: {:.3f} | cls_loss: {:.3f}'.format(loc_loss.data[0] / num_pos, cls_loss.data[0] / num_pos),
-        #       end=' | ')
-        # loss = (loc_loss + cls_loss) / num_pos
-        loc_loss = loc_loss
-        cls_loss = cls_loss / num_pos
-        return loc_loss, cls_loss
+def focalloss(pred_mask, pred_regr, mask, regr, weight=0.4, size_average=True, num_class=20):
+    # Binary mask loss
+    pred_mask = torch.sigmoid(pred_mask) # class masks
+    mask_loss = neg_loss(pred_mask, mask)
+    
+    sum=np.sum(mask.cpu().numpy()==1)
+    
+    # Regression L1 loss
+    #print(pred_regr.size())
+    #print(regr.size())
+    regr_loss = (torch.abs(pred_regr - regr).sum(1) * mask.sum(1)).sum(1).sum(1) / mask.sum(1).sum(1).sum(1)
+    regr_loss = regr_loss.mean(0)
+  
+    # Sum
+    loss = mask_loss +regr_loss
+    if not size_average:
+        loss *= prediction.shape[0]
+    return loss ,mask_loss , regr_loss
